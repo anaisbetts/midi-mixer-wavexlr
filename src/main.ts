3ed2b6ec8f23cdba58ceb4485ba9fdd190200160
Anaïs Betts <anais@anaisbetts.org>
2022-02-02 18:13:46 -0500

    Initial import

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,19 @@
-// public
-import { Assignment, ButtonType } from "midi-mixer-plugin";
+import { Assignment, ButtonType } from "midi-mixer-plugin"
+import WaveLinkClient from "./WaveLinkClient"
+import WebSocket from "ws"
 
-/**
- * Example of retrieving the plugin's settings.
- */
-$MM.getSettings().then((settings) => {
-  console.log("settings:", settings);
-});
+console.log(Assignment)
 
-/**
- * Example of setting up an assignment to be controlled by the plugin.
- */
-const example = new Assignment("foo", {
-  name: "Example Plugin Entry",
-});
+setTimeout(() => {
+  console.log("HELLO")
+  const wnd = globalThis as any
+  wnd.WebSocket = WebSocket
+  wnd.WaveLinkClient = new WaveLinkClient("windows")
+  wnd.WaveLinkClient.setAppIsRunning(true)
 
-example.on("volumeChanged", (level: number) => {
-  example.volume = level;
-});
+  $MM.showNotification("This is a test")
+}, 1000)
 
-example.on("mutePressed", () => {
-  example.muted = !example.muted;
-});
-
-example.on("assignPressed", () => {
-  example.assigned = !example.assigned;
-});
-
-example.on("runPressed", () => {
-  example.running = !example.running;
-});
-
-/**
- * Example of setting up a button type to be controlled by the plugin.
- */
-const typeExample = new ButtonType("bar", {
-  name: "Example Button Type",
-});
-
-typeExample.on("pressed", () => {
-  typeExample.active = !typeExample.active;
-});
-
-/**
- * Example of uses buttons and statuses to show the status of the plugin
- * on the settings page.
- *
- * We use the keys defined in the `settings` object of our `package.json` in
- * order to react to UI button presses and set resulting statuses.
- *
- * Using this is great for showing anything from "Connected" statuses to error
- * messages to your end users.
- */
-$MM.onSettingsButtonPress("run", () => {
-  $MM.setSettingsStatus("status", "Running...");
-
-  setTimeout(() => {
-    $MM.setSettingsStatus("status", "Done");
-  }, 1000);
-});
+setInterval(() => {
+  console.log("hi")
+}, 10000)
